727a2af8438c0d37505ab47ec7c122d49b6101ea
awfaa <awfa.a@live.iium.edu.my>
2023-01-17 12:39:46 +0800

    env file n vendor

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -88,7 +88,7 @@
     <div class="container d-flex h-100">
       <div class="row justify-content-center align-self-center" data-aos="fade-up">
         <div class="col-lg-6 intro-info order-lg-first order-last" data-aos="zoom-in" data-aos-delay="100">
-          <h2>Rapid Solutions<br>for Your <span>Business!</span></h2>
+          <h2>Study Zone<br>for Your <span>Learning!</span></h2>
           <div>
             <a href="#about" class="btn-get-started scrollto">Get Started</a>
           </div>
