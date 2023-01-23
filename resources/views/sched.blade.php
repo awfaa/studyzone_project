c31c9847f52a4725eccd5af9ce1b9877bc418bc5
wanmaiza <wmaizasayuti@gmail.com>
2023-01-23 18:12:30 +0800

    schedule box

--- resources/views/sched.blade.php
+++ resources/views/sched.blade.php
@@ -25,6 +25,8 @@
 <section id="schedule" class="sched-bg" style="background-color: #daf7da;">
     <div class="overlay-mf"></div>
 
+
+
     <div class="container" data-aos="fade-up">
       <div class="row">
         <div class="col-sm-12">
@@ -73,5 +75,62 @@
       </div>
     </div>
   </section><!-- End Schedule Section -->
+
+  <!-- ======= Services Section ======= -->
+  <section id="services" class="services section-bg">
+    <div class="container" data-aos="fade-up">
+
+      <header class="section-header">
+        <h3><span style="color:rgb(28, 124, 20)">Why Study Zone?</span></h3>
+        <p>Study Zone is a place you can organized all of your work and make sure everything is on track.</p>
+      </header>
+
+      <div class="row g-5">
+
+        {{-- <div class="col-md-6 col-lg-4 wow bounceInUp" data-aos="zoom-in" data-aos-delay="100">
+          <div class="box">
+            <div class="icon" style="background: #fceef3;"><i class="bi bi-briefcase" style="color: #ff689b;"></i></div>
+            <h4 class="title"><a href="">Schedule</a></h4>
+            <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+          </div>
+        </div> --}}
+        <div class="col-md-6 col-lg-12" data-aos="zoom-in" data-aos-delay="200">
+          <div class="box">
+
+            <div>
+                <table class="table table-hover">
+                    <thead>
+                      <tr>
+                        <th scope="col">Class Code</th>
+                        <th scope="col">Day</th>
+                        <th scope="col">Time</th>
+                        <th scope="col">Class Name</th>
+                        <th scope="col">Location</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($schedule as $sched)
+                      <tr>
+                        <td>{{ $sched->c_code }}</td>
+                        <td>{{ $sched->c_day }}</td>
+                        <td>{{ $sched->c_time }}</td>
+                        <td>{{ $sched->class_name }}</td>
+                        <td>{{ $sched->c_location }}</td>
+                      </tr>
+                      @endforeach
+                      <div>
+                        <a href="/add-schedule" class="btn-sched" style="background-color: #2a642a;">Add Class Schedule</a>
+                      </div>
+                    </tbody>
+                  </table>
+                </div>
+
+          </div>
+        </div>
+    </div>
+
+</div>
+</section><!-- End Services Section -->
+
 
 @endsection
