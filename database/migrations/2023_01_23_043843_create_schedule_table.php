1bb81e10eaa5f647b04aca228aa7ee693a302d9d
awfaa <awfa.a@live.iium.edu.my>
2023-01-23 19:12:10 +0800

    push migration

---
+++ database/migrations/2023_01_23_043843_create_schedule_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedule', function (Blueprint $table) {
+            $table->string('c_code', 20)->primary();
+            $table->foreignId('c_day', 10);
+            $table->string('c_time', 20);
+            $table->text('class_name', 20);
+            $table->longText('c_location', 20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedule');
+    }
+};
