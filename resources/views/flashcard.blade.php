c00e5aac09d6d8a605b61ee71538953f9111ec8a
nuramani393 <nuramani393@gmail.com>
2023-01-24 01:39:51 +0800

    Update flashcard.blade.php

--- resources/views/flashcard.blade.php
+++ resources/views/flashcard.blade.php
@@ -30,21 +30,12 @@
     <link rel="stylesheet" href="assets/css/flashcardstyle.css"/>
 </head>
 
-    <!-- ======= Flashcard Section ======= -->
+    {{-- <!-- ======= Flashcard Section ======= -->
     <section id="services" class="services section-bg">
         <div class="container" data-aos="fade-up">
           <header class="section-header">
             <h3><span style="color:rgb(28, 124, 20)">Flashcards</span></h3>
           </header>
-
-          {{-- <div class="col-md-6 col-lg-6" data-aos="zoom-in" data-aos-delay="200">
-            <div class="box">
-              <div class="icon" style="background: #fff0da;"><i class="bi bi-card-checklist" style="color: #e98e06;"></i></div>
-              <h4 class="title"><a href="/notes">Notes</a></h4>
-              <p class="description">Create and save your notes with Study Zone. Say no more to papers and ring binders and organize your notes like a pro!</p>
-            </div>
-          </div> --}}
-
           <div class="row g-5">
             <div class="col-md-6 col-lg-12" data-aos="zoom-in" data-aos-delay="200">
                 <div class="box">
@@ -97,7 +88,74 @@
             </div>
           </div>
         </div>
-    </section><!-- End Flashcrad Section -->
+    </section><!-- End Flashcrad Section --> --}}
+
+    <section id="contact" class="paralax-mf footer-paralax bg-image sect-mt4 route" style="background-image: url(assets/img/overlay-bg.jpg)">
+        <div class="overlay-mf"></div>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="contact-mf">
+                <div id="contact" class="box-shadow-full">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="title-box-2">
+                        <h5 class="title-left">
+                          List Of Flashcards
+                        </h5>
+                      </div>
+                      <div>
+                        <div class="container">
+                            <div class="add-flashcard-con">
+                              <button id="add-flashcard">Add Flashcard</button>
+                            </div>
+
+                            <!-- Display Card of Question And Answers Here -->
+                            <div id="card-con">
+                              <div class="card-list-container"></div>
+                            </div>
+                        </div>
+
+                          <!-- Input form for users to fill question and answer -->
+                          <div class="question-container hide" id="add-question-card">
+                            <h2>Add Flashcard</h2>
+                            <div class="wrapper">
+                              <!-- Error message -->
+                              <div class="error-con">
+                                <span class="hide" id="error">Input fields cannot be empty!</span>
+                              </div>
+                              <!-- Close Button -->
+                              <i class="fa-solid fa-xmark" id="close-btn"></i>
+                            </div>
+
+                            <label for="question">Question:</label>
+                            <textarea
+                              class="input"
+                              id="question"
+                              placeholder="Type the question here..."
+                              rows="2"
+                            ></textarea>
+                            <label for="answer">Answer:</label>
+                            <textarea
+                              class="input"
+                              id="answer"
+                              rows="4"
+                              placeholder="Type the answer here..."
+                            ></textarea>
+                            <button id="save-btn">Save</button>
+                          </div>
+
+                          <!-- Script -->
+                          <script src="assets/js/flashcard.js"></script>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </section><!-- End Flashcard Section -->
 
   @endsection
 
