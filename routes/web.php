110422621adda00b8ebb1b48cb5360306e064c33
wanmaiza <wmaizasayuti@gmail.com>
2023-01-23 23:18:21 +0800

    notesController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\notesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
