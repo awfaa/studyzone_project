8354ee31099aeb27d23767b252d175e70214fac9
Izzatyaris <zatyaris@gmail.com>
2023-01-24 02:35:25 +0800

    Update studytracker.blade.php

--- resources/views/studytracker.blade.php
+++ resources/views/studytracker.blade.php
@@ -6,9 +6,9 @@
     <div class="container d-flex h-100">
       <div class="row justify-content-center align-self-center" data-aos="fade-up">
         <div class="col-lg-12 intro-info order-lg-first order-last" data-aos="zoom-in" data-aos-delay="100">
-          <h2><span style="color:rgb(255, 255, 255)">Keep the focus with <br> Study Tracker </span> <span style="color:rgb(213, 248, 213)">Study Zone</span></h2>
+          <h2><span style="color:rgb(2, 55, 5)">Keep the focus with <br> Study Tracker </span> <span style="color:rgb(213, 248, 213)">Study Zone</span></h2>
           <div>
-            <a href="/sched" class="btn-get-started scrollto" style="background-color: #bded62;">My Study Tracker</a>
+            <a href="/flashcard" class="btn-get-started scrollto" style="background-color: #2a642a padding: 15px 32px; text-align: center; color: white;;">Study Tracker</a>
           </div>
         </div>
 
