7daf0ea792e27d1641e93cfe5da3dbda20647dfc
Tyra <athirahzaaba57@gmail.com>
2023-01-23 23:57:41 +0800

    notes.blade.php

--- resources/views/notes.blade.php
+++ resources/views/notes.blade.php
@@ -166,6 +166,9 @@ body{
  }
     </style>
 
+<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.bundle.min.js"></script>
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 <div class="page-content container note-has-grid">
     <ul class="nav nav-pills p-3 bg-white mb-3 rounded-pill align-items-center">
